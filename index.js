45f060723023400e8a0627a51a90d4f48e1d1fac
JuandiGo1 <98894997+JuandiGo1@users.noreply.github.com>
2025-04-02 15:29:45 -0500

    fix: solving cors

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ dotenv.config({ path: '.env' });
 const app = express();
 
 // Habilitar CORS para todas las rutas
+console.log(process.env.FRONTEND_URL)
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://127.0.0.1:8080",
-    credentials: true
-  }));
+  origin: process.env.FRONTEND_URL || "http://127.0.0.1:8080",
+  credentials: true
+}));
+
+
+app.options("*", cors());  // Maneja preflight requests
+
 
 
 app.use(cookieParser());
@@ -23,6 +28,6 @@ app.use(express.json());
 connectDB();
 
 app.use(routerUser);
-app.use("/task",routerTasks);
+app.use("/task", routerTasks);
 
 app.listen(process.env.PORT ?? 5000, () => console.log(`Servidor corriendo en el puerto ${process.env.PORT || 5000}`))
